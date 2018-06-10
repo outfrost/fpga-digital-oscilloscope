fcbdbaa8d57cef0169504c2e2901beb682781a25
outfrost <kotlet.bahn@gmail.com>
2018-06-10 04:15:39 +0200

    Add test benches for SampleMemory and VGACtl

---
+++ SampleMemoryTest.vhd
@@ -0,0 +1,102 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+
+ENTITY SampleMemoryTest IS
+END SampleMemoryTest;
+
+ARCHITECTURE behavior OF SampleMemoryTest IS
+	
+	-- Component Declaration for the Unit Under Test (UUT)
+	
+	COMPONENT SampleMemory
+	PORT(
+		Clk : IN  std_logic;
+		Write_Enable : IN  std_logic;
+		Write_Addr : IN  std_logic_vector(9 downto 0);
+		Write_Data : IN  std_logic_vector(8 downto 0);
+		Read_Addr : IN  std_logic_vector(9 downto 0);
+		Read_Data : OUT  std_logic_vector(8 downto 0)
+	);
+	END COMPONENT;
+	
+	
+	--Inputs
+	signal Clk : std_logic := '0';
+	signal Write_Enable : std_logic := '0';
+	signal Write_Addr : std_logic_vector(9 downto 0) := (others => '0');
+	signal Write_Data : std_logic_vector(8 downto 0) := (others => '0');
+	signal Read_Addr : std_logic_vector(9 downto 0) := (others => '0');
+	
+	--Outputs
+	signal Read_Data : std_logic_vector(8 downto 0);
+	
+	-- Clock period definitions
+	constant Clk_period : time := 20 ns;
+	
+BEGIN
+	
+	-- Instantiate the Unit Under Test (UUT)
+	uut: SampleMemory PORT MAP (
+		Clk => Clk,
+		Write_Enable => Write_Enable,
+		Write_Addr => Write_Addr,
+		Write_Data => Write_Data,
+		Read_Addr => Read_Addr,
+		Read_Data => Read_Data
+	);
+	
+	-- Clock process definitions
+	Clk_process :process
+	begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
+	end process;
+	
+	
+	-- Stimulus process
+	stim_proc: process
+	begin
+		-- hold reset state for 100 ns.
+		wait for 100 ns;
+		
+		Write_Addr <= "0000110010";
+		Write_Data <= "001101011";
+		
+		wait for Clk_period;
+		
+		Write_Enable <= '1';
+		
+		wait for Clk_period;
+		
+		Write_Enable <= '0';
+		Read_Addr <= "0001000101";
+		
+		wait for Clk_period*4;
+		
+		Write_Addr <= "0000111001";
+		Write_Data <= "001100110";
+		
+		wait for Clk_period;
+		
+		Write_Enable <= '1';
+		
+		wait for Clk_period;
+		
+		Write_Enable <= '0';
+		
+		wait for Clk_period*4;
+		
+		Read_Addr <= "0000110010";
+		
+		wait for Clk_period*10;
+		
+		wait;
+	end process;
+	
+END;
---
+++ VGACtlTest.vhd
@@ -0,0 +1,93 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+
+ENTITY VGACtlTest IS
+END VGACtlTest;
+
+ARCHITECTURE behavior OF VGACtlTest IS
+	
+	-- Component Declaration for the Unit Under Test (UUT)
+	
+	COMPONENT VGACtl
+	PORT(
+		PixelClk_50MHz : IN  std_logic;
+		Sample_Data : IN  std_logic_vector(8 downto 0);
+		Sample_Addr : OUT  std_logic_vector(9 downto 0);
+		VGA_R : OUT  std_logic;
+		VGA_G : OUT  std_logic;
+		VGA_B : OUT  std_logic;
+		VGA_HS : OUT  std_logic;
+		VGA_VS : OUT  std_logic
+	);
+	END COMPONENT;
+	
+	
+	--Inputs
+	signal PixelClk_50MHz : std_logic := '0';
+	signal Sample_Data : std_logic_vector(8 downto 0) := (others => '0');
+
+	--Outputs
+	signal Sample_Addr : std_logic_vector(9 downto 0);
+	signal VGA_R : std_logic;
+	signal VGA_G : std_logic;
+	signal VGA_B : std_logic;
+	signal VGA_HS : std_logic;
+	signal VGA_VS : std_logic;
+	
+	-- Clock period definitions
+	constant Clk_period : time := 20 ns;
+	
+BEGIN
+	
+	-- Instantiate the Unit Under Test (UUT)
+	uut: VGACtl PORT MAP (
+		PixelClk_50MHz => PixelClk_50MHz,
+		Sample_Data => Sample_Data,
+		Sample_Addr => Sample_Addr,
+		VGA_R => VGA_R,
+		VGA_G => VGA_G,
+		VGA_B => VGA_B,
+		VGA_HS => VGA_HS,
+		VGA_VS => VGA_VS
+	);
+	
+	-- Clock process definitions
+	Clk_process :process
+	begin
+		PixelClk_50MHz <= '0';
+		wait for Clk_period/2;
+		PixelClk_50MHz <= '1';
+		wait for Clk_period/2;
+	end process;
+	
+	
+	-- Stimulus process
+	stim_proc: process
+	begin
+		-- hold reset state for 100 ns.
+		wait for 100 ns;
+		
+		for i in 1 to 256 loop
+			if (Sample_Addr(1 downto 0) = "00") then
+				Sample_Data <= "001101011";
+			elsif (Sample_Addr(1 downto 0) = "01") then
+				Sample_Data <= "001100110";
+			elsif (Sample_Addr(1 downto 0) = "10") then
+				Sample_Data <= "000101001";
+			else
+				Sample_Data <= "000110101";
+			end if;
+			
+			wait for Clk_period;
+		end loop;
+		
+		wait for Clk_period*10;
+		
+		wait;
+	end process;
+	
+END;
